781dabe4eccc4eada1ce5bf26b05522290e599a5
edw7777 <wanged1110@gmail.com>
2025-01-29 14:48:55 -0500

    mongodb testing

--- main.py
+++ main.py
@@ -152,10 +152,7 @@ async def test_db():
 @app.get("/test-db-collection")
 async def test_db():
     client = get_mongo_client()
-    db = client["userdb"]
-    users_collection = db["credentials"]
-    for user in users_collection.find():
-        print(user)
+    print("Connected to:", client.server_info())
 
 
 @app.post("/api/register")
